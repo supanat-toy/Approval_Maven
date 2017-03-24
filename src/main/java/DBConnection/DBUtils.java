a9086647cc341920b34e0dbaf7f7c8dbbd455ac7
VAIO\SONY <mineo_w@outlook.co.th>
2017-03-24 14:33:46 +0700

    db connection

--- src/main/java/DBConnection/DBUtils.java
+++ src/main/java/DBConnection/DBUtils.java
@@ -19,29 +19,35 @@ import java.util.List;
  * Video:  https://www.youtube.com/watch?v=DN3VAZdhLik
  */
 public class DBUtils {
+    public static Connection getConnection(){
+        if(connection != null){
+            return connection;
+        }else{
+            try {
+                String driver = "com.mysql.jdbc.Driver";
+                String url = "jdbc:mysql://localhost:3306/approval_enterprise";
+                String user = "root";
+                String password = "root";
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+    }
+    
     public static PreparedStatement getPreparedStatement(String sql){
         PreparedStatement ps = null;
         Connection con = null;
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://ap-cdbr-azure-southeast-b.cloudapp.net:3306/approval_enterprise";
-            //String url = "jdbc:mysql://ap-cdbr-azure-southeast-b.cloudapp.net;databaseName=approval_enterprise";
-            String user = "b6f663e592256f";
-            String pass = "e8d63265";
-        
-            con = DriverManager.getConnection(url, user, pass);
+            con = getConnection();
             ps = con.prepareStatement(sql);
         }catch(Exception e){
-            
+            e.printStackTrace();
         }
         return ps;
-    }
-    
-    public static void main(String[] args) throws ClassNotFoundException, SQLException{
-        List<FormGroupModel> results = FormProvider.getAllRequests(8);
-        //List<FormModel> results = FormProvider.getForms(1);
-        System.out.print(results.size());
-        //System.out.print(results.get(0).getForm_group_id() + ":");
-        //System.out.print(results.get(0).getBasic_form().getCoordinator_name());
     }
 }
