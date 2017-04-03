1da4268cc9ac3707d79074c4224b665cca44a43e
ToyStory <supanat_toy@hotmail.com>
2017-04-03 20:57:55 +0700

    no message

--- src/main/java/Providers/LoginProvider.java
+++ src/main/java/Providers/LoginProvider.java
@@ -5,6 +5,14 @@
  */
 package Providers;
 
+import DBConnection.DBUtils;
+import Models.mResponse;
+import Models.mResult;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author supanattechasothon
@@ -12,5 +20,42 @@ package Providers;
 public class LoginProvider {
     
     public final int COORDINATOR = 1, SUPERVISOR = 2, ADMIN = 3, PROPERTIES = 4, TECHNICAL = 5, SOUNDANDLIGHT = 6, ARTSANDCULTURE = 7, SECURITY = 8, IT = 9;
+    
+    public mResult Authorization(String username, String password) {
+        mResult result = new mResult();
+        
+        String queryStatement = "select password user where user.username=?";
+        String query_password = "";
+        try{
+            PreparedStatement ps = DBUtils.getPreparedStatement(queryStatement);
+            ps.setString(1, username);
+            //ps.setString(2, password);
+            
+            ResultSet queryResult = ps.executeQuery();
+
+            while(queryResult.next()){            
+               query_password = queryResult.getString("password");
+               
+               break;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    
+        if (query_password == ""){
+            result.setIsSuccess(false);
+            result.setMessage("Username is wrong");
+        }
+        else if (query_password != password){
+            result.setIsSuccess(false);
+            result.setMessage("Password is wrong");
+        }
+        else {
+            result.setIsSuccess(true);
+            result.setMessage("Logic successfully");
+        }  
+        
+        return result;
+    }
     
 }
